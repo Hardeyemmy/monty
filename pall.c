5b710e82078eedca34d503f1c026b4b05acc471b
Hardeyemmy <iiadeyemi307@gmail.com>
2022-06-19 00:18:07 -0700

    pall.c

--- pall.c
+++ pall.c
@@ -1,24 +1,21 @@
 #include "monty.h"
-
 /**
- * f_pall - prints all the values on the stack,
- * starting from the top of the stack.
- * @stack: head of linked list (stack)
- * @line_number: counter lines
- */
-void f_pall(stack_t **stack, unsigned int line_number)
+ * f_pall - prints the stack
+ * @head: stack head
+ * @counter: no used
+ * Return: no return
+*/
+void f_pall(stack_t **head, unsigned int counter)
 {
-	stack_t *temp = *stack;
-	(void)line_number;
+	stack_t *h;
+	(void)counter;
 
-	if (stack == NULL || *stack == NULL)
+	h = *head;
+	if (h == NULL)
 		return;
-
-	while (temp->next != NULL)
-		temp = temp->next;
-	while (temp != NULL)
+	while (h)
 	{
-		printf("%d\n", temp->n);
-		temp = temp->prev;
+		printf("%d\n", h->n);
+		h = h->next;
 	}
 }
